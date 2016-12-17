7f67f76b7f3f5909c3d7691111112ac05feda752
Hari Vedam <hvedam@gmail.com>
2016-12-17 10:58:19 -0600

    working on post functionality of the blog

--- main.py
+++ main.py
@@ -16,26 +16,29 @@
 import os
 import webapp2
 import jinja2
+import re
+
+from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
+
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
-
+# Blogging Function
 class BaseHandler(webapp2.RequestHandler):
     """
-    BaseHandler is method that inherits from webapp2.
-    Helper Methods are to define.
+
     """
     def write(self, *a, **kw):
         """
         Writes output to client browser
         """
         self.response.out.write(*a, **kw)
-        
+
     def render_str(self, template, **params):
         """
         Render HTML Templates
@@ -45,9 +48,75 @@ class BaseHandler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+def render_post(response, post):
+    response.out.write('<b>' + post.subject + '<b><br>')
+    response.out.write('post.content')
+
+def blog_key(name='default'):
+    return db.Key.from_path('blogs', name)
+
+
 class MainPage(BaseHandler):
     def get(self):
-        self.render('default.html')
+        posts = db.GqlQuery("select * from Post order by created desc limit 10")
+        self.render('front.html', post= posts)
 
 
-app = webapp2.WSGIApplication([('/', MainPage),], debug=True)
+# Post Function
+class Post(db.model):
+    """
+    Attributes for the Post datastore
+    """
+    # userid = db.IntegerProperty(required=True)
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
+
+
+class PostPage(BaseHandler):
+    def get(self, post_id):
+        """
+        Renders Posts to home page
+        """
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+    def post(self, post_id):
+        """
+        Loops through the posts
+        """
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+class NewPostPage(BaseHandler):
+    def get(self):
+        self.render("newpost.html")
+
+    def post(self):
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+
+        if subject and content:
+            newpost = Post(parent = blog_key(), subject = subject, content = content)
+            newpost.put()
+            self.redirect('/post/%s' %str(newpost.key().id()))
+        else:
+            error = "Please enter Subject and Content"
+            self.render("newpost.html", subject= subject, content = content, error = error)
+
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/blog/([0-9]+)', PostPage)
+                              ], debug=True)
